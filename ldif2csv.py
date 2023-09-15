e6e7d37276e26b6be07b52e5ef171247f90b6d4c
João Luiz Lagreca da Silva <joao.lsilva.ext@telefonica.com>
2023-09-15 12:02:32 -0300

    Verifica se o arquivo existe antes de começar

--- ldif2csv.py
+++ ldif2csv.py
@@ -8,3 +8,10 @@ prs.add_argument('INPUT', help='LDIF input file')
 prs.add_argument('COLUMN', help='Columns to parse to CSV file', nargs='+')
 args = prs.parse_args()
 
+if not os.path.exists(args.INPUT):
+    sys.stderr.write("Input file doesn't exists")
+    exit(129)
+if os.path.isdir(args.INPUT):
+    sys.stderr.write("The input specified is a directory")
+    exit(123)
+
